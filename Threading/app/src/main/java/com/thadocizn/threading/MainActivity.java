8c0c6d73eb4745fb36cb8ae06cff647d4d72073e
ThaDocIzN <rodan72@msn.com>
2018-11-26 18:06:19 -0600

    added functionality to override methods.

--- Threading/app/src/main/java/com/thadocizn/threading/MainActivity.java
+++ Threading/app/src/main/java/com/thadocizn/threading/MainActivity.java
@@ -1,5 +1,6 @@
 package com.thadocizn.threading;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,37 +20,72 @@ public class MainActivity extends AppCompatActivity {
 
         msg = findViewById(R.id.textViewMsg);
         shift = findViewById(R.id.editTextShift);
-        progressBar = findViewById(R.id.progressBar);
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String strMsg = msg.getText().toString();
-                int shifted = Integer.parseInt(shift.getText().toString());
-                progressBar.setVisibility(View.VISIBLE);
+                new Task().execute();
 
-                if (strMsg != null) {
-                    String newText = "";
-                    for (int i = 0; i < strMsg.length(); ++i) {
-                        char unicode = strMsg.charAt(i);
-                        if ((unicode >= 'A' && unicode <= 'Z') || ((unicode >= 'a' && unicode <= 'z'))) {
-                            for (int n = 0; n < shifted; ++n) {
-                                ++unicode;
-                                if (unicode > 'Z' && unicode < 'a') {
-                                    unicode = 'A';
-                                }
-                                if (unicode > 'z') {
-                                    unicode = 'a';
-                                }
-                            }
-                        }
-                        newText += unicode;
-                        msg.setText(newText);
-
-                    }
-                }
-                progressBar.setVisibility(View.GONE);
             }
         });
+
+    }
+
+    public class Task extends AsyncTask<Void, Integer, String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progressBar = findViewById(R.id.progressBar);
+            progressBar.setVisibility(View.GONE);
+            msg.setText(s);
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            msg.setText(values[0].toString());
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String strMsg = msg.getText().toString();
+            int shifted = Integer.parseInt(shift.getText().toString());
+
+            if (strMsg != null) {
+                String newText = "";
+                for (int i = 0; i < strMsg.length(); ++i) {
+                    char unicode = strMsg.charAt(i);
+                    if ((unicode >= 'A' && unicode <= 'Z') || ((unicode >= 'a' && unicode <= 'z'))) {
+                        for (int n = 0; n < shifted; ++n) {
+                            ++unicode;
+                            if (unicode > 'Z' && unicode < 'a') {
+                                unicode = 'A';
+                            }
+                            if (unicode > 'z') {
+                                unicode = 'a';
+                            }
+                        }
+                    }
+                    newText += unicode;
+
+                }
+                return newText;
+            }
+            return null;
+        }
     }
 }
