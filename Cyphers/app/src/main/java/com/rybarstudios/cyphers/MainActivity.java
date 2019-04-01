9e6510d44acfe4501f3c4ca7e0f03ce09a7797eb
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-04-01 16:02:30 -0400

    Built Algo for shifting text

--- Cyphers/app/src/main/java/com/rybarstudios/cyphers/MainActivity.java
+++ Cyphers/app/src/main/java/com/rybarstudios/cyphers/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     Button shiftButton;
     EditText mEditText;
     TextView mTextView;
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +22,36 @@ public class MainActivity extends AppCompatActivity {
 
         mEditText = findViewById(R.id.editText);
         mTextView = findViewById(R.id.text_view);
+        mProgressBar = findViewById(R.id.progressBar);
         shiftButton = findViewById(R.id.button);
         shiftButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mProgressBar.setVisibility(View.VISIBLE);
                 String content = mTextView.getText().toString();
                 int charsToShift = Integer.parseInt(mEditText.getText().toString());
+                mTextView.setText(shift(content, charsToShift));
+                mProgressBar.setVisibility(View.GONE);
             }
         });
     }
 
-    private String shift(String content, int numToShift) {
-        //TODO write algo to shift the content by n number of characters
+    private StringBuilder shift(String content, int numToShift) {
+        StringBuilder builder = new StringBuilder();
 
-        return content;
+        for(int i = 0; i < content.length(); i++) {
+            if(Character.isUpperCase(content.charAt(i))) {
+                char ch = (char)(((int)content.charAt(i) +
+                        numToShift - 65) % 26 + 65);
+                builder.append(ch);
+            }else if(Character.isLowerCase(content.charAt(i))){
+                char ch = (char)(((int)content.charAt(i) +
+                        numToShift - 97) % 26 + 97);
+                builder.append(ch);
+            }else {
+                builder.append(content.charAt(i));
+            }
+        }
+        return builder;
     }
 }
