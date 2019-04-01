ee218f48cfb6d632f79480098810a3b8bac824f3
Spencer Stock <spencerstock@gmail.com>
2019-04-01 14:20:30 -0600

    basic cipher functional

--- app/src/main/java/com/example/android_threads/MainActivity.java
+++ app/src/main/java/com/example/android_threads/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.android_threads;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button shiftButton = findViewById(R.id.button);
+        final EditText editText = findViewById(R.id.edit_text);
+        final TextView textView = findViewById(R.id.text_view);
+
+        shiftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(cipher(textView.getText().toString(), Integer.parseInt(editText.getText().toString())));
+            }
+        });
+    }
+
+    String cipher(String msg, int shift){
+        String temp = "";
+        int length = msg.length();
+        for(int i = 0; i < length; i++){
+            char character = (char)(msg.charAt(i) + shift);
+            if (character > 'z')
+                temp += (char)(msg.charAt(i) - (26-shift));
+            else
+                temp += (char)(msg.charAt(i) + shift);
+        }
+        return temp;
     }
 }
