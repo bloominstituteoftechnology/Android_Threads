c0899b37e9cc66c33e875230b66ffbd47652ea3e
VivekV95 <vivekv210@gmail.com>
2019-04-01 17:16:22 -0500

    Moved decryption algorithm to Async background method

--- app/src/main/java/com/vivekvishwanath/android_threads/MainActivity.java
+++ app/src/main/java/com/vivekvishwanath/android_threads/MainActivity.java
@@ -1,5 +1,6 @@
 package com.vivekvishwanath.android_threads;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -7,12 +8,17 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     Button shiftButton;
     TextView cipherTextView;
     EditText userInput;
+    ProgressBar progressBar;
+    int shift;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,27 +28,59 @@ public class MainActivity extends AppCompatActivity {
         cipherTextView = findViewById(R.id.cipher_data_text);
         userInput = findViewById(R.id.shift_input);
         shiftButton = findViewById(R.id.shift_button);
+        shift = Integer.parseInt(userInput.getText().toString());
+        progressBar = findViewById(R.id.progress_bar);
         shiftButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String cipher = cipherTextView.getText().toString();
-                int shift = Integer.parseInt(userInput.getText().toString());
-                String newString = "";
-                char c;
-                for (int i = 0; i < cipher.length(); i++) {
-                    c = cipher.charAt(i);
-                    if (Character.isLetter(c)) {
-                        c = (char) (cipher.charAt(i) + shift);
-                    }
-                    if (Character.isLowerCase((cipher.charAt(i))) && c > 'z'
-                    || Character.isUpperCase(cipher.charAt(i)) && c > 'Z') {
-                        c = (char) (cipher.charAt(i) - (26 - shift));
-                    }
-                    newString += c;
-                }
-                System.out.println(newString);
             }
         });
 
+    }
+
+    class DecryptCypherAsync extends AsyncTask<String, Integer, String> {
+        @Override
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String newString = "";
+            char c;
+            for (int i = 0; i < strings[0].length(); i++) {
+                c = strings[0].charAt(i);
+                if (Character.isLetter(c)) {
+                    c = (char) (strings[0].charAt(i) + shift);
+                }
+                    /*if (Character.isLowerCase((cipher.charAt(i))) && c > 'z'
+                    || Character.isUpperCase(cipher.charAt(i)) && c > 'Z') {
+                        c = (char) (cipher.charAt(i) - (26 - shift));
+                    } */
+
+                if (strings[0].charAt(i) > 'Z' - shift || strings[0].charAt(i) > 'z' - shift) {
+                    c = (char) (strings[0].charAt(i) + shift);
+                }
+                newString += c;
+            }
+           // System.out.println(newString);
+            return newString;
+        }
     }
 }
